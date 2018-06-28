19fcc131aa6b2b671ab68da16f743f27cbcdc1da
Sven Bock @P48 <sb@generationrobots.com>
2018-06-28 02:26:56 +0200

    goog

--- Counting.py
+++ Counting.py
@@ -7,12 +7,46 @@ import cv2 as cv
 
 cv.namedWindow("tracking")
 camera = cv.VideoCapture("2018-06-27-151049.webm")
-cv.namedWindow("tracking")        # Create a named window
-cv.moveWindow("tracking", 0,0)  # Move it to (40,30)
-cv.namedWindow("tracking2")        # Create a named window
-cv.moveWindow("tracking2", 0,250)  # Move it to (40,30)
-cv.namedWindow("tracking3")        # Create a named window
-cv.moveWindow("tracking3", 0,300)  # Move it to (40,30)
+cv.namedWindow("1")        # Create a named window
+cv.moveWindow("1", 0,0)  # Move it to (40,30)
+cv.namedWindow("2")        # Create a named window
+cv.moveWindow("2", 0,250)  # Move it to (40,30)
+cv.namedWindow("3")        # Create a named window
+cv.moveWindow("3", 0,500)  # Move it to (40,30)
+
+ok, image=camera.read()
+blank_image = np.zeros((400/2,image.shape[1]/2,3), np.uint8)
+if not ok:
+    print('Failed to read video')
+    exit()
+cropped=image[(image.shape[0]-400):image.shape[0],0:image.shape[1]] 
+resized=cv.resize(cropped,((cropped.shape[1]/2),(cropped.shape[0]/2)),interpolation = cv.INTER_AREA)
+gray = cv.cvtColor(resized, cv.COLOR_BGR2GRAY) 
+gray = cv.GaussianBlur(gray, (21, 21), 0)
+firstFrame = gray
+ret,binary = cv.threshold(gray,120,255,cv.THRESH_BINARY_INV)
+#binary=cv.adaptiveThreshold(gray,255,cv.ADAPTIVE_THRESH_MEAN_C,cv.THRESH_BINARY_INV,20,2)
+
+img_contour, contours_new, hierarchy = cv.findContours(binary,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)    
+cv.drawContours(blank_image, contours_new, -1, (0,0,255), 2)
+
+# suppression des petite surface
+contours_final=[]
+for i in range(0,len(contours_new)-1):
+    if cv.contourArea(contours_new[i])>10.0:
+        contours_final.append(contours_new[i])
+        x,y,w,h = cv.boundingRect(contours_new[i])
+        resized = cv.rectangle(resized,(x,y),(x+w,y+h),(255,0,0),2)
+    #print i,cv.contourArea(contours[i])
+    
+    
+cv.drawContours(blank_image, contours_final, -1, (0,255,0), 2)
+cv.imshow("1", resized)
+cv.imshow("2", binary)
+cv.imshow("3", blank_image)    
+k = cv.waitKey(200)
+
+contours_old=contours_new
 
 while camera.isOpened():
     ok, image=camera.read()
@@ -20,26 +54,36 @@ while camera.isOpened():
     if not ok:
         print('Failed to read video')
         exit()
-
     cropped=image[(image.shape[0]-400):image.shape[0],0:image.shape[1]] 
     resized=cv.resize(cropped,((cropped.shape[1]/2),(cropped.shape[0]/2)),interpolation = cv.INTER_AREA)
-    gray = cv.cvtColor(resized, cv.COLOR_BGR2GRAY)  
-    ret,binary = cv.threshold(gray,127,255,cv.THRESH_BINARY)
-    img_contour, contours, hierarchy = cv.findContours(binary,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)    
-    cv.drawContours(blank_image, contours, -1, (0,0,255), 2)
-  
-    cv.imshow("tracking", resized)
-    cv.imshow("tracking2", gray)
-    cv.imshow("tracking3", blank_image)    
-    k = cv.waitKey(200)
+    gray = cv.cvtColor(resized, cv.COLOR_BGR2GRAY) 
+    gray = cv.GaussianBlur(gray, (21, 21), 0)
+    frameDelta = cv.subtract(firstFrame, gray)
+    firstFrame = gray
+    binary = cv.threshold(frameDelta,25,255,cv.THRESH_BINARY_INV)[1]
+    binary = cv.dilate(binary, None, iterations=2)
+    #binary=cv.adaptiveThreshold(gray,255,cv.ADAPTIVE_THRESH_MEAN_C,cv.THRESH_BINARY_INV,20,2)
+    
+    img_contour, contours_new, hierarchy = cv.findContours(binary,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)    
+    cv.drawContours(blank_image, contours_new, -1, (0,0,255), 2)
+    
+    contours_final=[]
+    for i in range(0,len(contours_new)-1):
+        if cv.contourArea(contours_new[i])>10.0:
+         #   if contours_new[i] in contours_old == True:
+            contours_final.append(contours_new[i])
+            x,y,w,h = cv.boundingRect(contours_new[i])
+            resized = cv.rectangle(resized,(x,y),(x+w,y+h),(255,0,0),2)
+                #print i,cv.contourArea(contours[i])
+    contours_old=contours_new
+        
+    cv.drawContours(blank_image, contours_final, -1, (0,255,0), 2)
+    cv.imshow("1", resized)
+    cv.imshow("2", binary)
+    cv.imshow("3", blank_image)    
+    k = cv.waitKey(500)
     
     
-#    lower_color = np.array([30,150,50])
-#    upper_color = np.array([255,255,180])
-#    
-#    mask = cv2.inRange(hsv, lower_red, upper_red)
-#    res = cv2.bitwise_and(frame,frame, mask= mask)
-#
-#    cv2.imshow('frame',frame)
-#    cv2.imshow('mask',mask)
-#    cv2.imshow('res',res)
+    
+    
+    
