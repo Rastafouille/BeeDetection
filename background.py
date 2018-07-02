b6690de471ae02cd6f4d431b8fb3dd5dad1165dd
rastafouille <jseyssaud@yahoo.fr>
2018-07-02 14:36:35 +0200

    add

---
+++ background.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture('"2018-06-27-151434.webm"')
+
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
+fgbg = cv2.bgsegm.createBackgroundSubtractorMOG()
+
+while(1):
+    ret, frame = cap.read()
+
+    fgmask = fgbg.apply(frame)
+    fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
+
+    cv2.imshow('frame',fgmask)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
---
+++ fenetreTest.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+import tkinter
+
+top = tkinter.Tk()
+# Code to add widgets will go here...
+top.mainloop()
+
